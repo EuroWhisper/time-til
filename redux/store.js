9d8c424c13ea37c334ed36ad76d0139208af20ba
EuroWhisper <ralphjuden@gmail.com>
2019-12-16 12:11:54 +0100

    Fixed bug with redux dev tools causing a server crash by trying to access non-existant window object.

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+let store;
+
+// window does not exist in the NodeJS runtime environment
+// so only load Redux devtools on the client-side.
+if (typeof window !== 'undefined') {
+    store = createStore(rootReducer, 
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());    
+} else {
+    store = createStore(rootReducer);
+}
+
+export default store;
